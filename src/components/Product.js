d4f2b7112513ce32a753dc38e7a449b1c17c3cd7
rejsby <urejsby@gmail.com>
2022-01-22 15:30:53 +0100

    Co-authored-by: elktelomete <elktelomete@users.noreply.github.com>

---
+++ src/components/Product.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Product(props) {
+    const {product, onAdd } = props;
+  return (
+    <div>
+        <h3>{product.name}</h3>
+        <div>${product.price}</div>
+        <div>
+            <button onClick={() => onAdd(product)}>Add to Cart</button>
+        </div>
+    </div>
+  )
+}
---
+++ src/data.js
@@ -0,0 +1,56 @@
+const data = {
+  products: [
+    {
+      id: 1,
+      name: "Royal Canin Veterinary Canine Hypoallergenic 2kg",
+      price: 249,
+    },
+    {
+      id: 2,
+      name: "Royal Canin Veterinary Canine Hypoallergenic 7kg",
+      price: 549,
+    },
+    {
+      id: 3,
+      name: "Royal Canin Mini Puppy 2kg",
+      price: 189,
+    },
+    {
+      id: 4,
+      name: "Royal Canin Mini Puppy 4kg",
+      price: 319,
+    },
+    {
+      id: 5,
+      name: "Purenatural Cat Sterilised Senior Turkey 2kg",
+      price: 299,
+    },
+    {
+      id: 6,
+      name: "Purenatural Cat Sterilised Senior Turkey 10kg",
+      price: 549,
+    },
+    {
+      id: 7,
+      name: "Purenatural Kitten Grain Free Turkey 2kg",
+      price: 299,
+    },
+    {
+      id: 8,
+      name: "Purenatural Kitten Grain Free Turkey 10kg",
+      price: 749,
+    },
+    {
+      id: 9,
+      name: "Trixie Trimsax",
+      price: 199,
+    },
+    {
+      id: 10,
+      name: "Trixie Universal Trimskrapa Fin 5x18 cm",
+      price: 179,
+    },
+  ],
+};
+
+export default data;
